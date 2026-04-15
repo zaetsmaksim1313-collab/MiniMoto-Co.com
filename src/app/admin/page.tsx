1252ce77c40f8daca569db5040b0fe346908a858
Maksim Zaets <maksimzaets@maksims-air.lan>
2026-04-14 17:47:54 -0700

    Fix dashboard create product button

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -52,6 +52,8 @@ export default function AdminPage() {
         if (res.success) {
             alert(editingProduct ? 'Product updated!' : 'Product added!');
             window.location.reload();
+        } else {
+            alert("Error: " + res.error);
         }
     };
 
